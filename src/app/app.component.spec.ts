b5dfa7525836cab40785cfcc258f39f0a4877549
Russ Carver <russ.carver@transamerica.com>
2019-05-21 01:20:01 -0600

    First solution.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,25 +8,62 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  describe('constructor', () => {
+    it('should create the app', () => {
+      const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+      const app: AppComponent = fixture.debugElement.componentInstance;
+      expect(app).toBeDefined();
+    });
   });
 
-  it(`should have as title 'ftr-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ftr-test');
+  describe('startTimer', () => {
+    it('should ', () => {
+    });
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ftr-test!');
+  describe('pause', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('quit', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('clearFib', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('addInput', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('isFibonacci', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('updateSummary', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('startRefreshTimer', () => {
+    it('should ', () => {
+    });
+  });
+
+  describe('stopRefreshTimer', () => {
+    it('should ', () => {
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ftr-test';
+export class AppComponent implements OnDestroy {
+  @ViewChild('userInput') public userInput: ElementRef;
+
+  public error: string;
+  public frequency: number = 0;
+  public pauseButtonText: string = 'Halt';
+  public running: boolean = false;
+  public showDoneMessage: boolean = false;
+  public showFib: boolean = false;
+  public started: boolean = false;
+  public summary: string = '';
+
+  private refreshTimer: Subscription;
+  private userInputs: Object = {}; // Hash
+
+  public ngOnDestroy(): void {
+    this.stopRefreshTimer();
+  }
+
+  public startTimer(): void {
+    this.showDoneMessage = false;
+    this.summary = '';
+    this.userInputs = {};
+    this.started = true;
+    this.running = true;
+    this.startRefreshTimer();
+  }
+
+  public pause(): void {
+    this.running = !this.running;
+    this.showFib = false;
+    this.pauseButtonText = this.running ? 'Halt' : 'Resume';
+    if (this.pauseButtonText === 'Resume') {
+      this.stopRefreshTimer();
+      this.summary = '';
+    } else {
+      this.startRefreshTimer();
+    }
+  }
+
+  public quit(): void {
+    this.started = false;
+    this.showFib = false;
+    this.stopRefreshTimer();
+    this.updateSummary();
+    this.showDoneMessage = true;
+  }
+
+  public clearFib(): void {
+    this.showFib = false;
+  }
+
+  private addInput(): void {
+    this.error = undefined;
+    this.showFib = false;
+    const value: number = parseInt(this.userInput.nativeElement.value, 10);
+    if (isNaN(value) || value <= 0) {
+      this.error = 'Value must be greater than zero';
+      return;
+    }
+
+    if (this.isFibonacci(value)) {
+      this.showFib = true;
+    }
+
+    const count: number = this.userInputs[`${value}`];
+    if (count) {
+      this.userInputs[`${value}`] = count + 1;
+    } else {
+      this.userInputs[`${value}`] = 1;
+    }
+    this.userInput.nativeElement.value = '';
+  }
+
+  private isFibonacci(num: number): boolean {
+    const root5: number = Math.sqrt(5);
+    const phi: number = (root5 + 1) / 2;
+    const idx: number = Math.floor(Math.log(num * root5) / Math.log(phi) + 0.5);
+    const u: number = Math.floor(Math.pow(phi, idx) / root5 + 0.5);
+    return (u === num);
+  }
+
+  private updateSummary(): void {
+    this.summary = '';
+    const arr: string[] = [];
+
+    for (const key in this.userInputs) {
+      arr.push(key);
+    }
+    for (let i: number = arr.length - 1; i >= 0; i--) {
+      this.summary += `${arr[i]}:${this.userInputs[arr[i]]}, `;
+    }
+
+    if (this.summary.length > 0) {
+      this.summary = this.summary.substr(0, this.summary.length - 2);
+    }
+  }
+
+  private startRefreshTimer(): void {
+    this.stopRefreshTimer();
+    this.refreshTimer = interval(this.frequency * 1000).subscribe(() => {
+      this.updateSummary();
+    });
+  }
+
+  private stopRefreshTimer(): void {
+    if (this.refreshTimer) {
+      this.refreshTimer.unsubscribe();
+    }
+  }
+
 }
